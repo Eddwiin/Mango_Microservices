d89acbc40a15641cf218704355ee0415c9c4831f
eddwiin <maguran.mahendrarajan@gmail.com>
2023-11-24 22:07:31 +0100

    first commit

---
+++ Mango_Microservices/Mango.Service.CouponAPI/Migrations/20231124195330_SeedCouponTables.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Service.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCouponTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DiscountAmount",
+                table: "Coupons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "LastUpdate", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "180FF", 10, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 20 },
+                    { 2, "200FF", 20, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 40 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DiscountAmount",
+                table: "Coupons",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
