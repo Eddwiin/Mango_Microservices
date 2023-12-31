96be1ee5a378195f122db12d2e16c3eb062c98f2
Maguran MAHENDRARAJAN <maguran.mahendrarajan@gmail.com>
2023-12-31 14:23:38 +0100

    Add ShoppingCart API

---
+++ microservices/Mango/Mango.Services.ShoppingCartAPI/Services/CouponService.cs
@@ -0,0 +1,32 @@
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mango.Services.ShoppingCartAPI.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CouponService(IHttpClientFactory clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+        }
+
+
+        public async Task<CouponDto> GetCoupon(string couponCode)
+        {
+            var client = _httpClientFactory.CreateClient("Coupon");
+            var response = await client.GetAsync($"/api/coupon/GetByCode/{couponCode}");
+            var apiContext = await response.Content.ReadAsStringAsync();
+            var resp = JsonConvert.DeserializeObject<ResponseDto>(apiContext);
+
+            if (resp.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(resp.Result));
+            }
+
+            return new CouponDto();
+        }
+    }
+}
